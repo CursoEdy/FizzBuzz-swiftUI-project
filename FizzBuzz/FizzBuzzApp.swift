53cbb579401e8a1090f9046a2b9f2c11a67beaee
edy alves <30.edyalves@gmail.com>
2024-11-20 16:58:56 -0300

    Initial Commit

---
+++ FizzBuzz/FizzBuzzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FizzBuzzApp.swift
+//  FizzBuzz
+//
+//  Created by ednardo alves on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FizzBuzzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
